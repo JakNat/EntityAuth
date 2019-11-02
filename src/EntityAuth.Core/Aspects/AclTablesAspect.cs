59eefc22ac54375ebc69ec9daea4779aa947fb7a
kuba.natonek998 <kuba.natonek998@gmail.com>
2019-11-02 23:17:37 +0100

    Added AclTables attribute and AclTables aspect for modeling ACL tables

---
+++ src/EntityAuth.Core/Aspects/AclTablesAspect.cs
@@ -0,0 +1,44 @@
+﻿using AspectInjector.Broker;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace EntityAuth.Core.Aspects
+{
+    [Aspect(Scope.PerInstance)]
+    public class AclTablesAspect
+    {
+        [Advice(Kind.After, Targets = Target.Method)]
+        public void SetTables(
+            [Argument(Source.Arguments)] object[] args,
+            [Argument(Source.Triggers)] Attribute[] attributes)
+        {
+            var aclAttribute = attributes
+                .Single(x => x.GetType().Equals(typeof(AclTablesAttribute)))
+                as AclTablesAttribute;
+
+            var idType = aclAttribute.Type;
+            
+            var modelBuilder = args[0] as ModelBuilder;
+
+
+            if (idType.Equals(typeof(int)))
+            {
+                modelBuilder.SetAclTables<int>();
+            }
+            else if (idType.Equals(typeof(long)))
+            {
+                modelBuilder.SetAclTables<long>();
+            }
+            else if (idType.Equals(typeof(Guid)))
+            {
+                modelBuilder.SetAclTables<Guid>();
+            }
+            else
+            {
+                throw new Exception("Identifier Type is not supported. " +
+                    "Supported types : {int, long, GUid}");
+            }
+        }
+    }
+}
---
+++ src/EntityAuth.Core/Attributes/AclTablesAttribute.cs
@@ -0,0 +1,24 @@
+﻿using AspectInjector.Broker;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityAuth.Core.Aspects
+{
+    /// <summary>
+    /// Generating acl tables:
+    /// <para> (EA_Resources, EA_Roles, EA_Permissions) </para>
+    /// <para> Attribute for DbContext.OnModelCreating method </para>
+    /// <para> Needed migration </para>
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Method)]
+    [Injection(typeof(AclTablesAspect))]
+    public class AclTablesAttribute : Attribute
+    {
+        public Type Type { get; set; }
+        public AclTablesAttribute(Type type)
+        {
+            Type = type;
+        }
+    }
+}
