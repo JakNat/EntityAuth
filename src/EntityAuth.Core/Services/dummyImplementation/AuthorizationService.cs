0c8da0198a4e875f0674ba16e0656d460e90172e
kuba.natonek998 <kuba.natonek998@gmail.com>
2019-11-02 23:22:21 +0100

    Created dummy AuthrozationService

---
+++ src/EntityAuth.Core/Services/dummyImplementation/AuthorizationService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EntityAuth.Core.Services
+{
+    /// <summary>
+    /// dummy auth service
+    /// need to discus how we authorize user
+    /// </summary>
+    public class AuthorizationService<T> : IAuthorizationService<T>
+    {
+        private T userId;
+
+        public AuthorizationService()
+        {
+            this.userId = default(T);
+        }
+
+        public T GetCurrentUserId()
+        {
+            return userId;
+        }
+
+        public void SetCurrentUser(T id)
+        {
+
+            userId = id;
+        }
+    }
+
+    public class DummyIntAuthorizationService : AuthorizationService<int>
+    {
+
+    }
+
+    public class DummyLongAuthorizationService : AuthorizationService<long>
+    {
+
+    }
+
+    public class DummyGuidAuthorizationService : AuthorizationService<Guid>
+    {
+
+    }
+}
