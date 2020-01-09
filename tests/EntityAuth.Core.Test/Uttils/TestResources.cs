b10827dc2f4a1c45a9529389fac9d665a8b2bf92
kuba.natonek998 <kuba.natonek998@gmail.com>
2020-01-09 15:09:51 +0100

    Added Uttils for testing purpose

---
+++ tests/EntityAuth.Core.Test/Uttils/TestResources.cs
@@ -0,0 +1,9 @@
+﻿namespace EntityAuth.Core.Test
+{
+    public static class TestResources
+    {
+        public static string Resource1 = "Resource1";
+        public static string Resource2 = "Resource2";
+        public static string Resource3 = "Resource3";
+    }
+}
