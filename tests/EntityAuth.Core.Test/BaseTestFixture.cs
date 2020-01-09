79baf59ce87b11389c6b2339b62d796b954ce787
kuba.natonek998 <kuba.natonek998@gmail.com>
2020-01-09 15:09:33 +0100

    Added BaseTestFixture

---
+++ tests/EntityAuth.Core.Test/BaseTestFixture.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using EntityAuth.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityAuth.Core.Test
+{
+    public class BaseTestFixture<T> : IDisposable
+    {
+        public DbContext Context { get; set; }
+
+        public BaseTestFixture()
+        {
+            var builder = new DbContextOptionsBuilder<TestDb<T>>()
+                    .UseInMemoryDatabase(databaseName: "database_name");
+
+            Context = new TestDb<T>(builder.Options);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
