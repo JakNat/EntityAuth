f73dde0812a49c4bb401be2f3e55160c2c17fc90
kuba.natonek998 <kuba.natonek998@gmail.com>
2020-01-09 15:10:22 +0100

    Added tests for recursive RoleRepository

---
+++ tests/EntityAuth.Core.Test/Repositories/HierarchyRepositoryTestFixture.cs
@@ -0,0 +1,51 @@
+﻿using EntityAuth.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace EntityAuth.Core.Test.Services
+{
+    public class HierarchyRepositoryTestFixture<T> : BaseTestFixture<T>
+    {
+        public HierarchyRepositoryTestFixture() : base()
+        {
+        }
+
+        public void ClearRoles()
+        {
+            var roles = Context.Set<Role>().ToList();
+            Context.Set<Role>().RemoveRange(roles);
+        }
+
+        public void SeedRoles()
+        {
+            var role = NewRole("Role1", 1).WithChildren(
+                       NewRole("Role11", 2),
+                       NewRole("Role12", 3).WithChildren(
+                           NewRole("Role121", 4),
+                           NewRole("Role122", 5),
+                           NewRole("Role123", 6).WithChildren(
+                               NewRole("Role1231", 7).WithChildren(
+                                   NewRole("Role12311", 8))))
+               );
+
+            var role2 = NewRole("Role2", 21).WithChildren(
+                            NewRole("Role21", 22),
+                            NewRole("Role22", 23).WithChildren(
+                                NewRole("Role221", 24).WithChildren(
+                                    NewRole("Role2211", 27).WithChildren(
+                                        NewRole("Role22111", 28))),
+                                NewRole("Role222", 25),
+                                NewRole("Role223", 26).WithChildren(
+                                    NewRole("Role2231", 29).WithChildren(
+                                        NewRole("Role22311", 30))))
+               );
+
+            Context.Set<Role>().AddRange(role, role2);
+            Context.SaveChanges();
+        }
+        private Role NewRole(string name, int id)
+        {
+            return new Role() { Name = name, Id = id };
+        }
+    }
+}
---
+++ tests/EntityAuth.Core.Test/Repositories/HierarchyRepositoryTests.cs
@@ -0,0 +1,94 @@
+﻿using EntityAuth.Core.Services;
+using EntityAuth.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace EntityAuth.Core.Test.Services
+{
+    /// <summary>
+    /// Testing hierarchical repository for role management 
+    /// </summary>
+    public class HierarchyRepositoryTests : IClassFixture<HierarchyRepositoryTestFixture<int>>
+    {
+        private readonly DbContext _db;
+        private readonly IRoleRepository _recursiveRepo;
+      
+        public HierarchyRepositoryTests(HierarchyRepositoryTestFixture<int> fixture)
+        {
+            _db = fixture.Context;
+
+            fixture.ClearRoles();
+            fixture.SeedRoles();
+
+            _recursiveRepo = new RoleRepository(_db);
+        }
+
+
+        [Theory]
+        [InlineData("Role1", 8)]
+        [InlineData("Role12", 6)]
+        [InlineData("Role123", 3)]
+        [InlineData("Role2", 10)]
+        [InlineData("Role22", 8)]
+        [InlineData("Role223", 3)]
+        public void GetRoleByName_Should_ReturnRoleAndItsOffspring(string roleName, int resultCount)
+        {
+            var result = _recursiveRepo.Get(x => x.Name == roleName);
+
+            Assert.Equal(resultCount, result.Count());
+        }
+
+        [Fact]
+        public void Add_ChildToRole1()
+        {
+            var newRole = new Role() { Name = "NewRole" };
+
+            _recursiveRepo.Add("Role1", newRole);
+            var result = _recursiveRepo.Get(x => x.Name == "Role1");
+
+            Assert.Equal(9, result.Count());
+        }
+
+        [Fact]
+        public void Add_ChildWithChildrenToRole1()
+        {
+            var newRole = new Role() { Name = "NewRole1" };
+            newRole.Children = new List<Role>()
+            {
+                new Role(){ Name = "NewRole2"},
+                new Role(){ Name = "NewRole3"}
+            };
+
+            _recursiveRepo.Add("Role1", newRole);
+            var result = _recursiveRepo.Get(x => x.Name == "Role1");
+
+            Assert.Equal(11, result.Count());
+
+            var result2 = _recursiveRepo.Get(x => x.Name == "NewRole1");
+
+            Assert.Equal(3, result2.Count());
+        }
+
+        [Theory]
+        [InlineData("Role1", "Role11", 7, 17)]
+        [InlineData("Role1", "Role123", 5, 15)]
+        [InlineData("Role12", "Role1231", 4, 16)]
+        [InlineData("Role2", "Role2", 0, 8)]
+        [InlineData("Role2", "Role22111", 9, 17)]
+        public void Remove_RoleByName_Should_RemoveRoleAndItsOffspring(string roleName, string roleNameToDelete,  int potomsNumber, int totalNumber)
+        {
+            _recursiveRepo.Delete(roleNameToDelete);
+
+            var result = _recursiveRepo.Get(x => x.Name == roleName);
+            var allRoles = _db.Set<Role>().ToList();
+
+            Assert.Equal(potomsNumber, result.Count());
+            Assert.Equal(totalNumber, allRoles.Count());
+        }
+
+    }
+}
